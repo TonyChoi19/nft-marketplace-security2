aae2786eda4803ed6f5e89661a31f2d1d26782f4
TonyChoi19 <82039408+TonyChoi19@users.noreply.github.com>
2022-04-17 22:15:48 +0800

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -2,26 +2,24 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import logo from '../assets/logo.png'
-import { AiOutlineSearch } from 'react-icons/ai'
 import toast, { Toaster } from 'react-hot-toast'
 
 const style = {
   wrapper: `bg-[#00000] flex flex-grow sm:flex-row m-4 justify-between items-center h-auto`,
   logoContainer: `flex items-center cursor-pointer`,
   logoText: ` ml-[1rem] text-black font-uber font-bold  text-2xl`,
-  searchBar: `flex flex-1 mx-[0.8rem] items-center bg-[#363840] border-2 border-[#363840] rounded-[0.8rem]`,
-  searchIcon: `text-gray-400 mx-3 font-bold text-lg`,
-  searchInput: `h-[2.6rem] w-full border-0 bg-transparent outline-0 ring-0 px-2 pl-0 text-white placeholder:text-gray-400`,
   headerItems: `flex items-center justify-end`,
   headerItem: `text-white px-4 font-black text-l text-[#7A0BC0] cursor-pointer hover:decoration-[#7A0BC0] hover:scale-110 duration-100`,
 }
 
 const Header = () => {
+  //Get user in rendering
   const [user, setUser] = React.useState({})
   React.useEffect(() => {
     setUser(JSON.parse(localStorage.getItem('g_userData')))
   }, [])
 
+  //Sign Out function
   const signOut = () => {
     setUser = {}
     localStorage.removeItem('g_userData')
@@ -30,30 +28,28 @@ const Header = () => {
   return (
     <div className={style.wrapper}>
       <Toaster />
+
+      {/* LOGO */}
       <Link href="/">
         <div className={style.logoContainer}>
           <Image src={logo} height={55} width={55} />
           <div className={style.logoText}>NEXT-NFTs</div>
         </div>
       </Link>
-      <div className={style.searchBar}>
-        <div className={style.searchIcon}>
-          <AiOutlineSearch />
-        </div>
-        <input
-          className={style.searchInput}
-          placeholder="Search items and collections"
-        />
-      </div>
+
+      {/* Nav Items */}
       <div className={style.headerItems}>
+        {/* Link to Collections */}
         <a className={style.headerItem} href="/Collections">
           Collections
         </a>
 
+        {/* Link to Owned NFT */}
         <a className={style.headerItem} href="/OwnedNFT">
           MyNFTs
         </a>
 
+        {/* Shows check Balance and sign out button, only user signed in */}
         {user && (
           <a
             className="text-l cursor-pointer px-4 font-black text-white text-red-500 underline duration-100 hover:scale-110 hover:decoration-[#7A0BC0]"
@@ -78,6 +74,7 @@ const Header = () => {
           </div>
         )}
 
+        {/* Show Sign in button, if user haven't sign in  */}
         {!user && (
           <div className={style.headerIcon}>
             <Link href="/Signin">
@@ -92,6 +89,7 @@ const Header = () => {
   )
 }
 
+// Check user balance from db
 async function checkBalance(userID) {
   var obj
   var balance
